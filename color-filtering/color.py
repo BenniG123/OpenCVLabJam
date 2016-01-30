01b0f12870ba30deab5caa220b0df8eec2d55052
Ben Williams <bww1@iastate.edu>
2016-01-30 17:07:53 -0600

    Trackbar support

--- color-filtering/color.py
+++ color-filtering/color.py
@@ -7,19 +7,42 @@ img = cv2.imread('pokemon.png', cv2.IMREAD_COLOR)
 # Convert BGR to HSV
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-# define range of blue color in HSV
-lower_blue = np.array([0,50,50])
-upper_blue = np.array([130,255,255])
+def nothing(x):
+    pass
 
-# Threshold the HSV image to get only blue colors
-mask = cv2.inRange(hsv, lower_blue, upper_blue)
+cv2.namedWindow('Color Filter Parameters');
 
-# Bitwise-AND mask and original image
-res = cv2.bitwise_and(img,img, mask= mask)
+# Create trackbars
+cv2.createTrackbar('H Max','Color Filter Parameters',0,255,nothing)
+cv2.createTrackbar('S Max','Color Filter Parameters',0,255,nothing)
+cv2.createTrackbar('V Max','Color Filter Parameters',0,255,nothing)
+cv2.createTrackbar('H Min','Color Filter Parameters',0,255,nothing)
+cv2.createTrackbar('S Min','Color Filter Parameters',0,255,nothing)
+cv2.createTrackbar('V Min','Color Filter Parameters',0,255,nothing)
 
-cv2.imshow('image',img)
-cv2.imshow('mask',mask)
-cv2.imshow('res',res)
-cv2.waitKey(5000)
+# lower_bounds = np.array([50,50,50])
+# upper_bounds = np.array([130,255,255])
+
+while True :
+
+    h_max = cv2.getTrackbarPos('H Max','Color Filter Parameters')
+    h_min = cv2.getTrackbarPos('H Mix','Color Filter Parameters')
+    s_max = cv2.getTrackbarPos('S Max','Color Filter Parameters')
+    s_min = cv2.getTrackbarPos('S Min','Color Filter Parameters')
+    v_max = cv2.getTrackbarPos('V Max','Color Filter Parameters')
+    v_min = cv2.getTrackbarPos('V Min','Color Filter Parameters')
+
+    # Threshold the HSV image
+    mask = cv2.inRange(hsv, np.array([h_min, s_min, v_min]), np.array([h_max, s_max, v_max]))
+
+    # Bitwise-AND mask and original image
+    res = cv2.bitwise_and(img,img, mask= mask)
+
+    # cv2.imshow('image',img)
+    # cv2.imshow('mask',mask)
+    cv2.imshow('res',res)
+    k = cv2.waitKey(30) & 0xFF
+    if k == 27:
+        break
 
 cv2.destroyAllWindows()
